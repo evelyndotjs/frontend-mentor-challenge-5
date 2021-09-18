82a10901faa4af8cabbfd7c6a75f01d91481199d
Evelyn <83251574+evelyndotjs@users.noreply.github.com>
2021-09-19 01:03:29 +0200

    Remove unwanted comments

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
-//     background-image: url("images/icon-error.svg");
-//     background-repeat: no-repeat;
-//     background-position: center;
-//     background-position-x: 90%;
-//     background-size: 20px;
-//   }
-
-// $(".btn").on("click", () => {
-//     if ($(".first-name-form").val() == "" && $(".last-name-form").val() == "" && $(".email-form").val() == "" && $(".password-form").val() == "") {
-//         $(".error-text").show();
-//     } else if ($(".first-name-form").val() == "") {
-//         $(".first-name-error").show();
-//     } else if ($(".last-name-form").val() == "") {
-//         $(".last-name-error").show();
-//     } else if ($(".email-form").val() == "") {
-//         $(".email-error").show();
-//     } else if ($(".password-form").val() == "") {
-//         $(".password-error").show();
-//     } else {
-//         alert("Free trial claimed!")
-//     }
-// })
+$(".btn").on("click", () => {
+    if ($(".first-name-form").val() == "" && $(".last-name-form").val() == "" && $(".email-form").val() == "" && $(".password-form").val() == "") {
+        $(".error-text").show();
+    } else if ($(".first-name-form").val() == "") {
+        $(".first-name-error").show();
+    } else if ($(".last-name-form").val() == "") {
+        $(".last-name-error").show();
+    } else if ($(".email-form").val() == "") {
+        $(".email-error").show();
+    } else if ($(".password-form").val() == "") {
+        $(".password-error").show();
+    } else {
+        alert("Free trial claimed!")
+    }
+})
